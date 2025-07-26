b61d4ba772672d49e9e6ca139a6a8eac6a33eeee
naveen <notnaveen2@gmail.com>
2025-07-26 19:13:17 +0530

    hero page responsive fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import chromeIcon from "@/public/icons/chrome.png";
 export default function Home() {
   return (
-    <div className="h-screen w-screen px-6 pb-20 ">
+    <div className="h-screen w-screen px-6 relative pb-20 ">
       <Navbar />
       <div className="hero-bg h-full w-full rounded-b-3xl">
         <div className="flex grow pt-72 text-white items-center flex-col">
           <div
-            className="text-white items-center border border-hero-border rounded-full flex
+            className="text-white text-sm sm:text-base items-center border border-hero-border rounded-full flex
          gap-x-2 px-2 py-1 mb-3"
           >
             <Image
@@ -20,20 +20,23 @@ export default function Home() {
             />{" "}
             Now Available in Chrome
           </div>
-          <div className="text-center text-6xl font-semibold mb-5">
+          <div className="text-center text-3xl sm:text-4xl font-semibold mb-5">
             Your Browser to Notion, <br />
             Seamlessly
           </div>
-          <div className="text-center text-white mb-5  tracking-wide">
+          <div className="text-center text-white mb-5 text-sm tracking-wide">
             Highlight any text on the web and send it directly to your Notion
             workspace.
           </div>
           <div className="flex gap-x-5">
-            <button className="border-2 border-white cursor-pointer hover:bg-white hover:text-black transition-all duration-150 rounded-full px-4 text-xl py-2">
+            <button className="border-2 border-white cursor-pointer hover:bg-white hover:text-black transition-all duration-150 rounded-full px-4 text-lg sm:text-xl py-2">
               Add To Chrome
             </button>
           </div>
         </div>
+      </div>
+      <div className="text-white absolute bottom-7 left-0 right-0 text-center ">
+        (In development)
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,15 @@ import { useRouter } from "next/navigation";
 export default function Navbar() {
   const router = useRouter();
   return (
-    <div className=" absolute top-0 right-0 left-0 flex items-center h-fit border-b border-hero-border justify-between py-2 px-10">
-      <div className="text-white text-xl font-semibold">Note To Notion</div>
+    <div className=" absolute top-0 right-0 left-0 flex items-center h-fit border-b border-hero-border justify-between py-2 px-4 sm:px-10">
+      <div className="text-white text-lg sm:text-xl font-semibold">
+        Note To Notion
+      </div>
       <button
         onClick={() => {
           router.push("/auth");
         }}
-        className="border-2 cursor-pointer border-white/65 hover:border-transparent transition-all duration-250 ease-in-out px-5 py-[6px] font-semibold rounded-full text-black bg-white text-right hover:bg-white/85 "
+        className="border-2 text-sm sm:text-base cursor-pointer border-white/65 hover:border-transparent transition-all duration-250 ease-in-out px-5 py-[6px] font-semibold rounded-full text-black bg-white text-right hover:bg-white/85 "
       >
         Get Started
       </button>
